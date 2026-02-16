8d0262eef106049425dd0f9344809e6c6072e44b
cauaof_souza <cauasantos064@gmail.com>
2026-02-16 04:22:36 -0300

    Refatora hierarquia de DTOs mudando tipo da implementação e adicionando birthDate
    - Muda interface RegisterDTO para abstract class
    - agora as validações podem ser feitas uma só vez na classe pai, reaproveitando código só fazendo novas validações nos atributos exclusivos de DriverRegisterRequestDTO.
    - Agora a própria DTO concreta tem método que retorna seu User de acordo com role

--- backend/src/main/java/com/vanvan/dto/RegisterDTO.java
+++ backend/src/main/java/com/vanvan/dto/RegisterDTO.java
@@ -1,23 +1,63 @@
 package com.vanvan.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.vanvan.model.User;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
 
 @JsonTypeInfo(
-    use = JsonTypeInfo.Id.NAME,
-    include = JsonTypeInfo.As.EXISTING_PROPERTY,
-    property = "role",
-    visible = true
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
+        property = "role",
+        visible = true
 )
 @JsonSubTypes({
-    @JsonSubTypes.Type(value = RegisterRequestDTO.class, names = {"passenger", "admin"}),
-    @JsonSubTypes.Type(value = DriverRegisterRequestDTO.class, name = "driver")
+        @JsonSubTypes.Type(value = RegisterRequestDTO.class, names = {"passenger", "admin"}),
+        @JsonSubTypes.Type(value = DriverRegisterRequestDTO.class, name = "driver")
 })
-public sealed interface RegisterDTO permits RegisterRequestDTO, DriverRegisterRequestDTO {
-    String role();
-    String name();      // Vamos garantir que todos tenham nome
-    String email();
-    String password();
-    String cpf();
-    String phone();
+/*
+*  Pai comum para DTOs do sistema, com m
+* **/
+@Getter
+@Setter
+@NoArgsConstructor //todas as ferramentas para o Jackson funcionar
+@AllArgsConstructor
+public abstract class RegisterDTO {
+
+    @NotNull
+    @Size(min = 4, max = 120)
+    protected String name;
+
+    @Email
+    @NotBlank
+    protected String email;
+
+    @NotBlank
+    @Size(min = 8)//sem complicações aqui por ora
+    protected String password;
+
+    @CPF
+    @NotBlank
+    protected String cpf; // já validado pelo jakarta
+
+    @Pattern(regexp = "\\d{10,11}")
+    @NotBlank
+    protected String phone;
+
+    @NotBlank
+    protected String role;
+
+    @Past
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    protected LocalDate birthDate; //validado no service
+
+    public abstract User toEntity();
 }
--- backend/src/main/java/com/vanvan/dto/RegisterRequestDTO.java
+++ backend/src/main/java/com/vanvan/dto/RegisterRequestDTO.java
@@ -1,13 +1,33 @@
 package com.vanvan.dto;
 
-/*
-* Como há DTOs diferentes, cria-se essa "herança"
-* **/
-public record RegisterRequestDTO(
-    String name,
-    String email,
-    String password,
-    String cpf,
-    String phone,
-    String role
-) implements RegisterDTO {}
+import com.vanvan.enums.UserRole;
+import com.vanvan.model.Administrator;
+import com.vanvan.model.Passenger;
+import com.vanvan.model.User;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+public class RegisterRequestDTO extends RegisterDTO {
+
+    public RegisterRequestDTO(String name, String email, String password, String cpf, String phone, String role, LocalDate birthDate) {
+        super(name, email, password, cpf, phone, role, birthDate);
+    }
+
+    @Override
+    public User toEntity() {
+        UserRole parsedRole = UserRole.valueOf(role.toUpperCase());
+
+        return switch (parsedRole) {
+            case PASSENGER -> new Passenger(
+                    name, cpf, phone, email, password, birthDate
+            );
+            case ADMIN -> new Administrator(
+                    name, cpf, phone, email, password, birthDate
+            );
+            default -> throw new IllegalArgumentException("Invalid role: " + role);
+        };
+    }
+}
